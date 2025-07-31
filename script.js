25794bdb7f0e1dc1eab9028660ffd5cf36a2a862
dhruvilad1001 <dhruvilad1001@gmail.com>
2025-07-31 13:41:51 +0530

    Updated homepage layout and styles

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// let apiUrl="https://api.openweathermap.org/data/2.5/weather?&units=matric&q=";
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-// let apiKey="05acf6e13da9a0c821dc28c4c1f6079f";
 
 let temp=document.getElementById("temp")
 let city=document.getElementById("city")
@@ -13,36 +11,47 @@ let card=document.querySelector(".card")
 let weatherStatus=document.querySelector(".weatherStatus")
 let invalid=document.getElementById("invalid")
 
-async function checkWeather(ciity){
-    let response=await fetch(apiUrl + ciity + `&appid=${apiKey}`);
-    let data=await response.json();
-    // console.log(data);
+async function checkWeather(ciity)
+{
+     let response=await fetch(apiUrl + ciity + `&appid=${WEATHER_API_KEY}`);
+     let data=await response.json();
+     //console.log(data);
 
-    if(data.cod=="404"){
-        invalid.style="disply:block"
-    }
-    else{
-    temp.innerHTML= Math.round(data.main.temp) + "°C";
-    city.innerHTML= data.name;
-    humidity.innerHTML= data.main.humidity + "%";
-    wind.innerHTML= data.wind.speed + "Km/h";
+      if(data.cod=="404")
+        {
+          invalid.style="display:block"
+          card.style.height ="100px";
+        }
+     else
+     {
+        temp.innerHTML= Math.round(data.main.temp) + "°C";     
+        city.innerHTML= data.name;
+        humidity.innerHTML= data.main.humidity + "%";
+        wind.innerHTML= data.wind.speed + "Km/h";
+        invalid.style="display:none"
 
-    if(data.weather[0].main=="Clouds"){
-        image.src="cloud.svg"
-    }
-    else if(data.weather[0].main=="Rain"){
-         image.src="rain.svg"
-    }
-    else if(data.weather[0].main=="Clear"){
-        image.src="day.svg"
-   }
-   else if(data.weather[0].main=="Snow"){
-    image.src="snow.svg"
-}
+     if(data.weather[0].main=="Clouds")
+        {
+          image.src="cloud.svg"
+        }
+     else if(data.weather[0].main=="Rain")
+        {
+          image.src="rain.svg"
+        }
+      else if(data.weather[0].main=="Clear")
+        {
+         image.src="day.svg"
+        }
+     else if(data.weather[0].main=="Snow")
+        {
+         image.src="snow.svg"
+        }
 
-card.style="height:500px; transition:height 0.5s";
-weatherStatus.style="display:block"
+        card.style="height:500px; transition:height 0.5s";
+        weatherStatus.style="display:block"
+    }
 }
-}
-// checkWeather(search.value);
+ 
 btn.addEventListener("click",()=>{checkWeather(search.value)})
+
+
